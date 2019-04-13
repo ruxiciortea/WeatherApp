4bd7e2a940df3acc5646f692e6bba773dd9a9656
RuxiC <ciortearuxandra10@gmail.com>
2019-04-13 18:08:36 +0300

    Initial commit

---
+++ WeatherApp/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Ruxandra Ciortea on 13/04/2019.
+//  Copyright © 2019 Ruxandra Ciortea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WeatherApp/Model/Location.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  WeatherApp
+//
+//  Created by Ruxandra Ciortea on 13/04/2019.
+//  Copyright © 2019 Ruxandra Ciortea. All rights reserved.
+//
+
+import UIKit
+
+class Location: NSObject {
+
+}
